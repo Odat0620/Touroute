3adc65c64aa31ad55465f992e87e9fe361b30540
Odat0620 <oda620@icloud.com>
2021-07-10 01:54:28 +0900

    api/ postsコントローラーのアクション追加

--- api/app/controllers/v1/posts_controller.rb
+++ api/app/controllers/v1/posts_controller.rb
@@ -1,12 +1,12 @@
 class V1::PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :update, :destroy]
   def index
     @posts = Post.all
     render json: @posts
   end
 
   def show
-    render json: @post
+    render json: @post.as_json(include: { user: { only: :name } })
   end
 
   def create
@@ -21,9 +21,19 @@ class V1::PostsController < ApplicationController
   end
 
   def update
+    return if @post.user_id != post_params[:user_id]
+
+    if @post.update(post_params)
+      render json: @post, status: :ok
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    return if @post.user_id != params[:user_id]
+
+    @post.destroy
   end
 
   private
