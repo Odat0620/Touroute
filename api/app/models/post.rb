92ebde517d1e20e70f1c9f3c7dbf16ebbbbf1b7d
Odat0620 <oda620@icloud.com>
2021-07-10 20:57:53 +0900

    api/ アソシエーション追加 ・commentモデル作成によりアソシエーション追加

--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  # アソシエーション
   belongs_to :user
+  has_many :comments
+
+  # バリデーション
   validates :title, presence: true, length: { maximum: 20 }
 end
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
+  # アソシエーション
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  # バリデーション
+  validates :name,  presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true
 end
