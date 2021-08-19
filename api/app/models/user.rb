999802cf967557efa73016d88b2b4c3b31afadc0
Odat0620 <oda620@icloud.com>
2021-08-19 10:59:19 +0900

    api/ Userモデルにアソシエーション追加 ・relationshipモデル関係のアソシエーションを追加 ・フォロー関係のメソッド追加

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -7,9 +7,31 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
 
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers,  through: :reverse_of_relationships, source: :user
+
   # バリデーション
   validates :name,  presence: true, length: { maximum: 20 }
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+
+  # フォロー関係のメソッド
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_or_create_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
 
   #  すでにいいねしているか
   def already_liked?(post)
