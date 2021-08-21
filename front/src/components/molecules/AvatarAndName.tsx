ede38e33775d2d629e76177cec5abd6dcafc80ba
Odat0620 <oda620@icloud.com>
2021-08-21 10:38:56 +0900

    front/ AvatarAndNameコンポーネント作成 ・ユーザーのアバターと名前を表示 ・onClick指定する必要あり（ユーザーページへ遷移など）

---
+++ front/src/components/molecules/AvatarAndName.tsx
@@ -0,0 +1,27 @@
+import { Avatar, Flex, Text } from "@chakra-ui/react";
+import { VFC } from "react";
+
+type Props = {
+  name: string;
+  avatarUrl?: string;
+  onClick: () => void;
+};
+
+export const AvatarAndName: VFC<Props> = (props) => {
+  const { name, avatarUrl, onClick } = props;
+
+  return (
+    <Flex align="center" cursor="pointer" onClick={onClick}>
+      <Avatar
+        h="40px"
+        w="40px"
+        src={avatarUrl}
+        showBorder
+        borderColor="gray.300"
+      />
+      <Text mx={2} fontWeight="bold" color="gray.600">
+        {name}
+      </Text>
+    </Flex>
+  );
+};
