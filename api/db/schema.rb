df27e7f253cdbb40eeb7b6b9e10e5d431af198a5
Odat0620 <oda620@icloud.com>
2021-08-22 21:38:00 +0900

    api/ postモデルへimageカラム追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_065149) do
+ActiveRecord::Schema.define(version: 2021_08_21_053018) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "text", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_065149) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
